5abb55bd826877aebe5e6a396f2e2d4a8607fd3e
edem ADZAVON <edemadzav@gmail.com>
2020-06-28 16:14:06 +0000

    Updates

---
+++ precache-manifest.de443c0866af53e1f77d14366197ebe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fdabfc3e44b7e030222d3db5f0a0c2",
+    "url": "/deploy-sdm/index.html"
+  },
+  {
+    "revision": "dc12fc65189a30490014",
+    "url": "/deploy-sdm/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0e15a625dea47a17e080",
+    "url": "/deploy-sdm/static/css/main.d9eebe6a.chunk.css"
+  },
+  {
+    "revision": "dc12fc65189a30490014",
+    "url": "/deploy-sdm/static/js/2.d803f915.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deploy-sdm/static/js/2.d803f915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e15a625dea47a17e080",
+    "url": "/deploy-sdm/static/js/main.35f3e0c4.chunk.js"
+  },
+  {
+    "revision": "70b7d7ec24dec2243cde",
+    "url": "/deploy-sdm/static/js/runtime-main.51de6310.js"
+  }
+]);
